c3482f9d1ad78a15b8013af843f5f2069419cdd5
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-20 14:45:58 +0300

    написал код для решения задачи № 38

---
+++ Zadacha_38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+using static System.Console;
+Clear();
+
+int [] array = getArray(7, -10, 10);
+WriteLine($"{String.Join(" ", array)}");
+int max = differenceMaxNumbersArray(array);
+int min = differenceMinNumbersArray(array);
+int result = max - min;
+WriteLine($"Разница между max и min элементами массива равна {result}");
+
+int[] getArray(int size, int minValue, int maxValue)
+{
+    int[] rezult = new int[size];
+    for(int i = 0; i < rezult.Length; i++)
+    {
+        rezult[i] = new Random().Next(minValue, maxValue);
+    }
+    return rezult;
+}
+
+
+int differenceMaxNumbersArray (int[] array1)
+{
+    int max = array1[0];
+    for(int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i] > max) max = array1[i];
+    }
+    return max; 
+}
+
+int differenceMinNumbersArray (int[] array2)
+{
+    int min = array2[0];
+    for(int i = 0; i < array2.Length; i++)
+    {
+        if(array2[i] < min) min = array2[i];
+    }
+    return min;
+}
